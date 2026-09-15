3b92da3efd5d66a44e15885277ede061a276a774
Bill Odom <bill@billodom.com>
2026-09-15 13:27:24 -0400

    Clean up and refactor; add routerlinkactive directive

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,31 +1,16 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet, RouterLink } from '@angular/router';
+import {
+  RouterOutlet,
+  RouterLink,
+  RouterLinkActive,
+} from '@angular/router';
 
 @Component({
-  imports: [RouterOutlet, RouterLink],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive],
   selector: 'app-root',
   styleUrl: './app.scss',
   templateUrl: './app.html',
 })
 export class App {
   protected readonly title = signal('workshop-app');
-
-  constructor() {
-    setTimeout(() => {
-      this.title.set('The Timed Title');
-      console.log('Hey, I changed the title!');
-    }, 5000);
-  }
-
-  changeTitle() {
-    this.title.set('The New Title');
-  }
-
-  sayHello() {
-    console.log('Howdy!');
-  }
-
-  sayGoodbye() {
-    console.log('Bye :-(');
-  }
 }
