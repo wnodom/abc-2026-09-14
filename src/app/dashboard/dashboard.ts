74ee9534418e6c74794ef997d9f7f87937558725
Bill Odom <bill@billodom.com>
2026-09-15 14:09:26 -0400

    Add first draft of player and list

--- src/app/dashboard/dashboard.ts
+++ src/app/dashboard/dashboard.ts
@@ -1,7 +1,9 @@
 import { Component } from '@angular/core';
+import { VideoList } from './video-list/video-list';
+import { VideoPlayer } from './video-player/video-player';
 
 @Component({
-  imports: [],
+  imports: [VideoList, VideoPlayer],
   selector: 'ns-dashboard',
   styleUrl: './dashboard.scss',
   templateUrl: './dashboard.html',
