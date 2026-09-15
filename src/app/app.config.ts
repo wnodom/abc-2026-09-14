fd6086b58e6df9e1ae7309574656dfb0759db2f8
Bill Odom <bill@billodom.com>
2026-09-15 14:20:22 -0400

    Do cool stuff with the router

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,9 +2,24 @@ import {
   ApplicationConfig,
   provideBrowserGlobalErrorListeners,
 } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import {
+  PreloadAllModules,
+  provideRouter,
+  // withDebugTracing,
+  withPreloading,
+  withViewTransitions,
+} from '@angular/router';
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideBrowserGlobalErrorListeners(), provideRouter(routes)],
+  providers: [
+    provideBrowserGlobalErrorListeners(),
+
+    provideRouter(
+      routes,
+      withPreloading(PreloadAllModules),
+      withViewTransitions(),
+      // withDebugTracing(),
+    ),
+  ],
 };
