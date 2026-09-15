ebcf1c097f4c3e989cf5a1a1e5f7578d99cbb22b
Bill Odom <bill@billodom.com>
2026-09-15 13:36:45 -0400

    Add and route to dashboard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,10 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   {
+    path: 'dashboard',
+    loadComponent: () => import('./dashboard/dashboard'),
+  },
+  {
     path: 'about',
     loadComponent: () => import('./about/about'),
   },
