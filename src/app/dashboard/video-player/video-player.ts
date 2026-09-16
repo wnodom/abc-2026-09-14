3fefcf4493cd39f3b4364120443b4f3e1c061857
Bill Odom <bill@billodom.com>
2026-09-16 15:49:49 -0400

    Make the player work

--- src/app/dashboard/video-player/video-player.ts
+++ src/app/dashboard/video-player/video-player.ts
@@ -1,5 +1,6 @@
-import { Component, input } from '@angular/core';
+import { Component, computed, inject, input } from '@angular/core';
 import { Video } from '../../types';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   imports: [],
@@ -9,4 +10,21 @@ import { Video } from '../../types';
 })
 export class VideoPlayer {
   public readonly video = input.required<Video | undefined>();
+
+  // constructor(private sanitizer: DomSanitizer) {}
+
+  private readonly sanitizer = inject(DomSanitizer);
+
+  protected readonly videoUrl = computed(() => {
+    console.log('COMPUTING VIDEO URL');
+
+    const v = this.video();
+    if (v) {
+      return this.sanitizer.bypassSecurityTrustResourceUrl(
+        'https://www.youtube.com/embed/' + v.id,
+      );
+    }
+
+    return undefined;
+  });
 }
