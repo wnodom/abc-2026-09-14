4f95999f0533490181e770db85dc1baf6a8e79fc
Bill Odom <bill@billodom.com>
2026-09-17 12:07:14 -0400

    Move video data handling to service

--- src/app/dashboard/dashboard.ts
+++ src/app/dashboard/dashboard.ts
@@ -2,9 +2,9 @@ import { Component, inject, signal } from '@angular/core';
 import { VideoList } from './video-list/video-list';
 import { VideoPlayer } from './video-player/video-player';
 import { Video } from '../types';
-import { HttpClient } from '@angular/common/http';
 
 import { toSignal } from '@angular/core/rxjs-interop';
+import { VideoDataService } from '../video-data-service';
 
 @Component({
   imports: [VideoList, VideoPlayer],
@@ -15,18 +15,11 @@ import { toSignal } from '@angular/core/rxjs-interop';
 export default class Dashboard {
   protected readonly currentVideo = signal<Video | undefined>(undefined);
 
-  // private readonly http = inject(HttpClient);
+  private readonly svc = inject(VideoDataService);
 
-  // protected videos$ = inject(HttpClient).get<Video[]>(
-  //   'https://api.angularbootcamp.com/videos',
-  // );
-
-  protected readonly videos = toSignal(
-    inject(HttpClient).get<Video[]>(
-      'https://api.angularbootcamp.com/videos',
-    ),
-    { initialValue: [] },
-  );
+  protected readonly videos = toSignal(this.svc.loadVideos(), {
+    initialValue: [],
+  });
 
   tellMeAboutVideo(v: Video) {
     console.log('THE DASHBOARD SAYS...', v);
